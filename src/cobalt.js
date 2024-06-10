4ec2d2fc123413329ec7c6cae06a23d7fc0c179b
joc-luis <84199335+joc-luis@users.noreply.github.com>
2024-06-09 21:37:51 -0600

    fix: commit and branch name

--- src/cobalt.js
+++ src/cobalt.js
@@ -25,10 +25,10 @@ await loadLoc();
 
 if (mode === 'API') {
     const { runAPI } = await import('./core/api.js');
-    runAPI(express, app, gitCommit, gitBranch, __dirname)
+    runAPI(express, app, "gitCommit", "gitBranch", __dirname)
 } else if (mode === 'WEB') {
     const { runWeb } = await import('./core/web.js');
-    await runWeb(express, app, gitCommit, gitBranch, __dirname)
+    await runWeb(express, app, "gitCommit", "gitBranch", __dirname)
 } else {
     console.log(
         Red(`cobalt wasn't configured yet or configuration is invalid.\n`)
